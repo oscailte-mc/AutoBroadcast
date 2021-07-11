8a84b48f9cf46782000fbc4866e90823449eaed2
Rían Errity <rian@paradaux.io>
2021-07-11 02:09:30 +0100

    Add WIP locale system

---
+++ src/main/java/io/paradaux/autobroadcast/api/LocaleLogger.java
@@ -0,0 +1,28 @@
+package io.paradaux.autobroadcast.api;
+
+import org.slf4j.Logger;
+
+public class LocaleLogger {
+
+    private static Logger logger;
+
+    public LocaleLogger(Logger logger, LocaleManager locale) {
+        LocaleLogger.logger = logger;
+    }
+
+    public static void info(String str, String... args) {
+        logger.info(LocaleManager.get("str"), (Object[]) args);
+    }
+
+    public static void warn(String str, String... args) {
+        logger.warn(LocaleManager.get("str"), (Object[]) args);
+    }
+
+    public static void debug(String str, String... args) {
+        logger.debug(LocaleManager.get("str"), (Object[]) args);
+    }
+
+    public static void error(String str, String... args) {
+        logger.error(str, (Object[]) args);
+    }
+}
---
+++ src/main/java/io/paradaux/autobroadcast/api/LocaleManager.java
@@ -0,0 +1,14 @@
+package io.paradaux.autobroadcast.api;
+
+public class LocaleManager {
+
+    private static LocaleManager instance;
+
+    public static LocaleManager getInstance() {
+        return instance;
+    }
+
+    public static String get(String key) {
+        return "";
+    }
+}
